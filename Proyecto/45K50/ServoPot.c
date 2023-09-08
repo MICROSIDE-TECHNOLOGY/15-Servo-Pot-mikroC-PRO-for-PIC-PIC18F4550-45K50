9b386a0f99e64f70ae0844299fedf8912b410d29
vhperaltaj <victor.peralta@microside.com>
2023-09-08 10:33:56 -0600

    Fixed indentation

--- Proyecto/45K50/ServoPot.c
+++ Proyecto/45K50/ServoPot.c
@@ -16,7 +16,7 @@ Para usar el código con bootloader, configurar como lo indica MICROSIDE:
 2) Project> Edit Project> Insertar 48.000000 en "MCU Clock Frequency [MHz]" box
 ********************************************************************************/
 
-#pragma orgall 0x1FFF         //Reserva la memoria correspondiente al bootloader
+#pragma orgall 0x1FFF           // Reserva la memoria correspondiente al bootloader
 
 int ValorPOT;
 int i;
@@ -25,29 +25,23 @@ int i;
 
 void main() org 0x2000
 {
-   LATC2_bit = 0;                        //Se asigna el pin como salida
-   TRISC = 0;                            //Se establece el pin en un estado bajo
+   LATC2_bit = 0;               // Se asigna el pin como salida
+   TRISC = 0;                   // Se establece el pin en un estado bajo
    ANSELC = 0;
-   TRISA0_bit = 1;                       //PIN como entrada
-   ANSA0_bit = 1;                        //PIN como anal?gico
-   
-    while(1) 
-    {
-        ValorPOT=ADC_Read(0);            //Leer el valor del potenciometro
-           
-        for(i=0;i< ValorPOT + 100 ;i++)
-        {
-           pin_servo = 1;
-           Delay_us(1);
-        }
+   TRISA0_bit = 1;              // PIN como entrada
+   ANSA0_bit = 1;               // PIN como anal?gico
 
+   while ( 1 ) {
+      ValorPOT = ADC_Read( 0 ); // Leer el valor del potenciometro
 
-        for(i=0;i<20000 - ValorPOT;i++)
-        {
-           pin_servo = 0;
-           Delay_us(1);
-        }
-         
-    }
+      for ( i = 0; i < ValorPOT + 100; i++ ) {
+         pin_servo = 1;
+         Delay_us( 1 );
+      }
 
+      for ( i = 0; i < 20000 - ValorPOT; i++ ) {
+         pin_servo = 0;
+         Delay_us( 1 );
+      }
+   }
 }
